52679bdb02a3e4daac2c98fb3844ce8586c0e37c
yangxy2000 <yangxy_19910928@163.com>
2020-04-18 15:59:30 +0800

    创建新项目

---
+++ src/main/java/com/mr/reviewmr/ReviewmrApplication.java
@@ -0,0 +1,13 @@
+package com.mr.reviewmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewmrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewmrApplication.class, args);
+    }
+
+}
